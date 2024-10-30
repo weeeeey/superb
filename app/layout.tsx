79bfa6e136e35c1fd0534c89b389330fc9e6cf0e
위영진 <qser155@naver.com>
2024-10-30 15:55:05 +0900

    setup project structure and base configurations

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+
+const geistSans = localFont({
+    src: './fonts/GeistVF.woff',
+    variable: '--font-geist-sans',
+    weight: '100 900',
+});
+const geistMono = localFont({
+    src: './fonts/GeistMonoVF.woff',
+    variable: '--font-geist-mono',
+    weight: '100 900',
+});
+
+export const metadata: Metadata = {
+    title: '백만개의 체크박스',
+    description: '백만개의 체크박스가 포함된 컴포넌트를 최적화하기',
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="ko">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
---
+++ constants.ts
@@ -0,0 +1,6 @@
+const YELLOW = 'yellow';
+const RED = 'red';
+const GREEN = 'green';
+const PURPLE = 'purple';
+
+export { YELLOW, RED, GREEN, PURPLE };

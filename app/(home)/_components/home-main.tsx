dd01b2839abda3533899478826ac3ac0903d3252
위영진 <qser155@naver.com>
2024-10-30 17:45:52 +0900

    feat: add a handleClick function for checked box

--- app/(home)/_components/home-main.tsx
+++ app/(home)/_components/home-main.tsx
@@ -1,4 +1,11 @@
-export default function HomeMain() {
+import { GREEN, PURPLE, RED, YELLOW } from '@/constants';
+import { ColorType } from '../page';
+
+interface HomeMainProps {
+    handleClick: (isCheck: boolean, color?: ColorType) => void;
+}
+
+export default function HomeMain({ handleClick }: HomeMainProps) {
     return (
         <main className="h-full pt-20 bg-red-500 px-20 ">
             <section className="h-full bg-green-500 overflow-y-auto ">
@@ -12,9 +19,28 @@ export default function HomeMain() {
                                 className="hidden peer"
                                 type="checkbox"
                                 id={'' + idx}
+                                onChange={(e) => {
+                                    let color;
+                                    if (idx % 30 === 0) color = GREEN;
+                                    if (idx % 50 === 0) color = RED;
+                                    if (idx % 70 === 0) color = PURPLE;
+                                    if (idx % 90 === 0) color = YELLOW;
+
+                                    if (color)
+                                        handleClick(
+                                            e.target.checked,
+                                            color as ColorType
+                                        );
+                                    else handleClick(e.target.checked);
+                                }}
                             />
                             <label
-                                className="border border-black rounded-md size-4 peer-checked:bg-blue-600 transition-all cursor-pointer"
+                                className={`border-2 rounded-md size-4 peer-checked:bg-blue-600 transition-all cursor-pointer border-black
+                                    ${idx % 30 === 0 && 'border-green-800'}
+                                    ${idx % 50 === 0 && 'border-red-800'}
+                                    ${idx % 70 === 0 && 'border-purple-800'}
+                                    ${idx % 90 === 0 && 'border-yellow-800'}
+                                    `}
                                 htmlFor={'' + idx}
                             ></label>
                         </div>

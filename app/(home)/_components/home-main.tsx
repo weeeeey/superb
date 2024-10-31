23e1d1dcc94dc25539f32e6f807675c812248951
위영진 <qser155@naver.com>
2024-10-31 15:13:58 +0900

    refactor: 섹션 별 체크박스 갯수를 천개로 변경

--- app/(home)/_components/home-main.tsx
+++ app/(home)/_components/home-main.tsx
@@ -10,7 +10,7 @@ export default function HomeMain({ handleClick }: HomeMainProps) {
         <main className="h-full pt-20 bg-red-500 px-20 ">
             <section className="h-full bg-green-500 overflow-y-auto ">
                 <div className="grid grid-cols-[repeat(auto-fill,minmax(40px,1fr))] gap-1">
-                    {Array.from({ length: 1200 }).map((_, idx) => (
+                    {Array.from({ length: 1000 }).map((_, idx) => (
                         <div
                             key={idx}
                             className="flex items-center justify-center"

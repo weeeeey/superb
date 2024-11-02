a4c1a0d04410ee67628688a648583235700c2c39
위영진 <qser155@naver.com>
2024-11-02 19:41:13 +0900

    feat: apply tailwindCss prettierrc

--- app/(home)/_components/home-footer-button.tsx
+++ app/(home)/_components/home-footer-button.tsx
@@ -1,29 +1,29 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState } from "react";
 
 export default function HomeFooterButton({
-    selectTarget,
+  selectTarget,
 }: {
-    selectTarget: (inputValue: string) => void;
+  selectTarget: (inputValue: string) => void;
 }) {
-    const [inputValue, setInputValue] = useState('');
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        selectTarget(inputValue);
-    };
-    return (
-        <footer className="fixed bottom-5 right-5 min-x-[360px] flex justify-end ">
-            <form onSubmit={handleSubmit}>
-                <input
-                    className="border-4 border-blue-500 px-2 py-1"
-                    type="text"
-                    placeholder="checkbox number"
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                />
-                <button className="first-letter:uppercase bg-blue-500 text-white px-2 py-2">
-                    jump!
-                </button>
-            </form>
-        </footer>
-    );
+  const [inputValue, setInputValue] = useState("");
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    selectTarget(inputValue);
+  };
+  return (
+    <footer className="min-x-[360px] fixed bottom-5 right-5 flex justify-end">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="border-4 border-blue-500 px-2 py-1"
+          type="text"
+          placeholder="checkbox number"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button className="bg-blue-500 px-2 py-2 text-white first-letter:uppercase">
+          jump!
+        </button>
+      </form>
+    </footer>
+  );
 }
--- app/(home)/_components/home-header.tsx
+++ app/(home)/_components/home-header.tsx
@@ -1,26 +1,26 @@
-import { type CheckStateType, ColorType } from '../page';
+import { type CheckStateType, ColorType } from "../page";
 
 interface HomeHeaderProps {
-    checkState: CheckStateType;
+  checkState: CheckStateType;
 }
 
 export default function HomeHeader({ checkState }: HomeHeaderProps) {
-    return (
-        <header className="fixed top-0 left-0 w-full min-w-[360px] bg-slate-200 h-20 flex flex-col ">
-            <section className="flex justify-between items-center my-auto whitespace-nowrap gap-x-4">
-                <div>edited by 위영진</div>
-                <h1 className="font-extrabold text-xl">백만개의 체크 박스</h1>
-                <div>{checkState.checkedCount}의 박스가 체크 됨</div>
-            </section>
-            <ul className="flex items-center justify-end">
-                현재 {checkState.checkedCount} (
-                {Object.keys(checkState.colorCheckedCount).map((color) => (
-                    <li className="mx-2" style={{ color }} key={color}>
-                        {checkState.colorCheckedCount[color as ColorType]}
-                    </li>
-                ))}
-                )
-            </ul>
-        </header>
-    );
+  return (
+    <header className="fixed left-0 top-0 flex h-20 w-full min-w-[360px] flex-col bg-slate-200">
+      <section className="my-auto flex items-center justify-between gap-x-4 whitespace-nowrap">
+        <div>edited by 위영진</div>
+        <h1 className="text-xl font-extrabold">백만개의 체크 박스</h1>
+        <div>{checkState.checkedCount}의 박스가 체크 됨</div>
+      </section>
+      <ul className="flex items-center justify-end">
+        현재 {checkState.checkedCount} (
+        {Object.keys(checkState.colorCheckedCount).map((color) => (
+          <li className="mx-2" style={{ color }} key={color}>
+            {checkState.colorCheckedCount[color as ColorType]}
+          </li>
+        ))}
+        )
+      </ul>
+    </header>
+  );
 }

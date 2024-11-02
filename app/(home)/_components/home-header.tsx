ae481c6c635cfe49fe1b4b2c78bb7777aefb9a5b
위영진 <qser155@naver.com>
2024-11-02 19:53:47 +0900

    refactor: modify header style

--- app/(home)/_components/home-header.tsx
+++ app/(home)/_components/home-header.tsx
@@ -6,11 +6,11 @@ interface HomeHeaderProps {
 
 export default function HomeHeader({ checkState }: HomeHeaderProps) {
   return (
-    <header className="fixed left-0 top-0 flex h-20 w-full min-w-[360px] flex-col bg-slate-200">
+    <header className="fixed left-0 top-0 flex h-20 w-full min-w-[360px] flex-col bg-black px-2 text-white sm:px-5 md:px-10">
       <section className="my-auto flex items-center justify-between gap-x-4 whitespace-nowrap">
-        <div>edited by 위영진</div>
+        <div>위영진</div>
         <h1 className="text-xl font-extrabold">백만개의 체크 박스</h1>
-        <div>{checkState.checkedCount}의 박스가 체크 됨</div>
+        <div>{checkState.checkedCount}개의 박스가 체크</div>
       </section>
       <ul className="flex items-center justify-end">
         현재 {checkState.checkedCount} (
